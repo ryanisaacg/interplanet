f09cb331457354c104fe0be5018199bde401c313
Ryan Goldstein <ryanisaacg@gmail.com>
2017-07-16 15:41:45 -0400

    Fixed game title

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "controls.h"
 
 int main() {
-    AU_Engine* eng = au_init("interplanet", 800, 600, NULL, DEFAULT_CONFIG);
+    AU_Engine* eng = au_init("Interplanet", 800, 600, NULL, DEFAULT_CONFIG);
 	controls_loop(eng);
     au_quit(eng);
     return 0;
